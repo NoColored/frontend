4363b7b6d0396a6b895bca8ed423740d7d2fa706
damdam6 <dambi626626@gmail.com>
2024-03-08 18:02:54 +0900

    remove: example 코드 삭제
    conflict해결을 위한  example코드 삭제

--- frontend/src/pages/example/index.tsx
+++ frontend/src/pages/example/index.tsx
@@ -1,21 +1,12 @@
 import * as styles from './index.css.ts';
 
-<<<<<<< HEAD
-import ExampleScene from '@/scene/example-scene/index.tsx';
-=======
-import Game from '@/components/Game.tsx'
->>>>>>> f67893f0d323455281e03f81a722293594247bc3
+import Game from '@/components/Game.tsx';
 
 const Example = () => {
   return (
     <div>
       <div className={styles.exampleStyle}>Example</div>
-<<<<<<< HEAD
-      <ExampleScene />
-      <div className={styles.testStyle}>classsNameTest</div>
-=======
       <Game />
->>>>>>> f67893f0d323455281e03f81a722293594247bc3
     </div>
   );
 };
