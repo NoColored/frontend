bea3946344d08838e5d90d64af353e813cd69921
damdam6 <dambi626626@gmail.com>
2024-03-11 14:18:07 +0900

    feat: sprinkles 설정, globalTheme수정 >> >> sprinkles 설정을 통한 전체 스타일 사용 >> fontfamily지정 >> >> Related to :  #S10P22A608-100

--- frontend/src/styles/vars.css.ts
+++ frontend/src/styles/vars.css.ts
@@ -1,92 +1,127 @@
 import { createGlobalTheme } from '@vanilla-extract/css';
 
+import {
+  mainFont,
+  numFont,
+  loadingTextFont,
+  loadingNumFont,
+} from '@/styles/font.css.ts';
+
 export const vars = createGlobalTheme(':root', {
-    /* paddings, margins 대신 space 사용 */
-    space: {
-        none: '0',
-        '0.5x': '4px',
-        '1x': '8px',
-        '2x': '16px',
-        '3x': '24px',
-        '4x': '32px',
-        '5x': '40px',
-    },
+  /* paddings, margins 대신 space 사용 */
+  space: {
+    none: '0',
+    '0.5x': '4px',
+    '1x': '8px',
+    '2x': '16px',
+    '3x': '24px',
+    '4x': '32px',
+    '5x': '40px',
+  },
+  display: {
+    flex: 'flex',
+    block: 'block',
+    inlineBlock: 'inline-block',
+    inline: 'inline',
+  },
+  justifyContent: {
+    flexStart: 'flex-start',
+    center: 'center',
+    flexEnd: 'flex-end',
+    spaceBetween: 'space-between',
+    spaceAround: 'space-around',
+  },
+  alignItems: {
+    flexStart: 'flex-start',
+    center: 'center',
+    flexEnd: 'flex-end',
+    stretch: 'stretch',
+    baseline: 'baseline',
+  },
+  flexDirection: {
+    row: 'row',
+    rowReverse: 'row-reverse',
+    column: 'column',
+    columnReverse: 'column-reverse',
+  },
+  colors: {
+    white: '#ffffff',
+    black: '#000000',
+    red: '#F21F0C',
+    redDark: '#B6190A',
+    yellow: '#F2B705',
+    yellowDark: '#B68A05',
+    green: '#4CA732',
+    greenDark: '#397D26',
+    blue: '#04B1D9',
+    blueDark: '#0386A4',
+    pink: '#EB4180',
+    pinkDark: '#B13161',
+    navy: '#201D35',
+    navyDark: '#413F53',
+    gray: '#808080',
+    gray300: '#979797',
+    gray500: '#727272',
+    gray700: '#605F5F',
 
-    colors: {
-        white: '#ffffff',
-        black: '#000000',
-        red: '#F21F0C',
-        redDark : '#B6190A',
-        yellow: '#F2B705',
-        yellowDark: '#B68A05',
-        green: '#4CA732',
-        greenDark: '#397D26',
-        blue: '#04B1D9',
-        blueDark: '#0386A4',
-        pink: '#EB4180',
-        pinkDark: '#B13161',
-        navy: '#201D35',
-        navyDark: '#413F53',
-        gray: '#808080',
-        gray300: '#979797',
-        gray500: '#727272',
-        gray700: '#605F5F',
+    // 이하 기타 색들
+    lightGray: '#CDCDCD',
+    whitesmoke: '#f1f1f1',
+    babyBlue: '#BFCDE5',
+    emerald: '#74cf9c',
+    purple: '#cf8abc',
+    oatmeal: '#fdf5e4',
+    brown: '#453216',
+    strongRed: '#E8635C',
+    lightBlue: '#8AAEF9',
+    lavender: '#BBA4DA',
+  },
 
-        // 이하 기타 색들
-        lightGray: '#CDCDCD',
-        whitesmoke: '#f1f1f1',
-        babyBlue: '#BFCDE5',
-        emerald: '#74cf9c',
-        purple: '#cf8abc',
-        oatmeal: '#fdf5e4',
-        brown: '#453216',
-        strongRed: '#E8635C',
-        lightBlue: '#8AAEF9',
-        lavender: '#BBA4DA',
-    },
+  borderRadius: {
+    '0x': '0px',
+    '1x': '8px',
+    '2x': '16px',
+    '3x': '24px',
+    '4x': '32px',
+    '5x': '40px',
+    top: '32px 32px 0 0',
+    full: '9999px', // 원을 만들 때 사용
+  },
 
-    borderRadius: {
-        '0x': '0px',
-        '1x': '8px',
-        '2x': '16px',
-        '3x': '24px',
-        '4x': '32px',
-        '5x': '40px',
-        top: '32px 32px 0 0',
-        full: '9999px', // 원을 만들 때 사용
-    },
+  fontFamily: {
+    body: mainFont,
+    numFont,
+    loadingTextFont,
+    loadingNumFont,
+  },
 
-    fontFamily: {
-        body: 'Arial',
-    },
+  fontSize: {
+    '1x': '8px',
+    '1.5x': '12px',
+    '2x': '16px',
+    '3x': '24px',
+    '4x': '32px',
+    '5x': '40px',
+  },
 
-    fontSize: {
-        '1x': '8px',
-        '1.5x': '12px',
-        '2x': '16px',
-        '3x': '24px',
-        '4x': '32px',
-        '5x': '40px',
-    },
+  fontWeight: {
+    normal: '400',
+    accent: '700',
+  },
 
-    fontWeight: {
-        normal: '400',
-        accent: '700',
-    },
+  // lineHeight 는 필요할 때 사용
+  lineHeight: {},
 
-    // lineHeight 는 필요할 때 사용
-    lineHeight: {},
-
-    zIndex: {
-        'alert-content': '3001',
-        'alert-bg': '3000',
-        'content-to-top': '2001',
-        'content-bg-to-top': '2000',
-        'layout-content': '1000',
-        'z-5': '5',
-        'z-4': '4',
-        'z-3': '3',
-        'z-2': '2',
-        'z-1': '1',
-    },
+  zIndex: {
+    'alert-content': '3001',
+    'alert-bg': '3000',
+    'content-to-top': '2001',
+    'content-bg-to-top': '2000',
+    'layout-content': '1000',
+    'z-5': '5',
+    'z-4': '4',
+    'z-3': '3',
+    'z-2': '2',
+    'z-1': '1',
+  },
 });
