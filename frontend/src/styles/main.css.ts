14e976ee01fdb9bb9f2ff5b7de68c2a6526b36b2
김세진 <sejin1398@gmail.com>
2024-03-12 14:11:46 +0900

    fix: main.css 수정
    - 모든 태그에 border-box 적용
    - root 컴포넌트 border-size 1로 수정
    - root 컴포넌트 background-image 추가
    
    related to : #S10P22A608-153

--- frontend/src/styles/main.css.ts
+++ frontend/src/styles/main.css.ts
@@ -6,6 +6,7 @@ import { vars } from '@/styles/vars.css.ts';
 globalStyle('*', {
   margin: 0,
   padding: 0,
+  boxSizing: 'border-box',
 });
 
 globalStyle('html, body', {
@@ -29,14 +30,16 @@ globalStyle('#root', {
   width: '100%',
   aspectRatio: '33 / 19',
   display: 'block',
+  backgroundImage:
+    'url(images/background/background-white-small-h450w873.webp)',
 
   '@media': {
     'screen and (min-width: 748px)': {
       width: '748px',
-      border: '2px solid black',
+      border: '1px solid black',
       borderRadius: '8px',
       margin: `${calc('100vh').subtract('431px').divide(2).toString()} auto`,
-      overflow: 'hidden',
+      backgroundImage: 'none',
       backgroundColor: vars.colors.background,
     },
   },
