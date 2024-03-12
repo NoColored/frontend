53e211a18405fc4b702e1c5c85498d2f488110b2
damdam6 <dambi626626@gmail.com>
2024-03-12 13:30:48 +0900

    feat: example 주석 추가
    example 설정 추가
    
    related to : #S10P22A608-70

--- frontend/src/pages/example/index.css.ts
+++ frontend/src/pages/example/index.css.ts
@@ -4,18 +4,21 @@ import * as styleOptions from '@/styles/common.css.ts';
 import { sprinkles } from '@/styles/sprinkles.css.ts';
 
 export const testForRecipe = style([
+  // 임의 설정한 option들을 배열에 등록
   styleOptions.flexOptions({
     option: 'center',
   }),
   styleOptions.borderOptions({
     color: 'red',
-    width: '1x',
+    width: '5x',
   }),
+  // 추가적인 디테일은 sprinkles에서 사용
   sprinkles({
     fontFamily: 'textFont',
     fontSize: '2x',
     paddingX: '2x',
   }),
+  // 그외 추가 값
   {
     // 임시로 값 설정
     width: '300px',
