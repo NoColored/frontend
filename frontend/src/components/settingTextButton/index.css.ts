72b29d6fbecf30ebcc3e2fffe8d8107fee383514
minjeong <minj_1203@naver.com>
2024-03-12 11:21:19 +0900

    feat: 공통 컴포넌트 css 작업중

---
+++ frontend/src/components/settingTextButton/index.css.ts
@@ -0,0 +1,41 @@
+// import { style } from '@vanilla-extract/css';
+//
+// const settingButtonBase = style({
+//   lineHeight: '16px',
+//   color: 'black',
+//   border: 'solid 2px black',
+//   borderRadius: '8px',
+//   backgroundColor: 'white',
+// });
+//
+// const settingButtonVariants = {
+//   size: {
+//     small: {
+//       fontsize: '16px',
+//       padding: '8px',
+//       height: '24px',
+//       width: '60px',
+//     },
+//     large: {
+//       fontsize: '20px',
+//       padding: '8px',
+//       height: '38px',
+//       width: '211px',
+//     },
+//   },
+//   colorStyle: {
+//     black: {},
+//     navy: { backgroundColor: 'navy', color: 'white' },
+//     red: { borderColor: 'red' },
+//     grey: { borderColor: 'grey' },
+//   },
+// };
+//
+// const colors: ('black' | 'navy' | 'red' | 'grey')[] = [
+//   'black',
+//   'navy',
+//   'red',
+//   'grey',
+// ];
+//
+// const
