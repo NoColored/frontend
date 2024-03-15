0094af122ee3edb14d3be9d9fd57fc127e22171f
Gardener <str-pes@naver.com>
2024-03-15 13:02:26 +0900

    feat : ColoredButton Constant Variables 추가
    constant.ts 추가에 따른 css 파일 변경함.
    
    Related to : S10P22A608-99

--- frontend/src/components/button/ColoredButton/index.css.ts
+++ frontend/src/components/button/ColoredButton/index.css.ts
@@ -1,29 +1,29 @@
 import { style } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
 
-import * as constants from '@/components/button/constants.ts';
+import * as constants from '@/components/button/ColoredButton/constants.ts';
 
 import { flexOptions } from '@/styles/common.css.ts';
 import { sprinkles } from '@/styles/sprinkles.css.ts';
 import { vars } from '@/styles/vars.css.ts';
 
 const colorVariants: {
-  [colorItem: string]: ReturnType<typeof style>;
-} = {
-  '': style([]),
-};
+  [colorItem in (typeof constants.COLOREDBUTTON_COLOR)[number]]?: ReturnType<
+    typeof style
+  >;
+} = {};
 
-constants.COLOREDICONBUTTON_COLOR.forEach((colorItem) => {
-  const darkColor = constants.COLOREDICONBUTTON_COLOR_DARK[colorItem];
+constants.COLOREDBUTTON_COLOR.forEach((colorItem) => {
+  const darkColor = constants.COLOREDBUTTON_COLOR_DARK[colorItem];
   colorVariants[colorItem] = style([
     sprinkles({
       backgroundColor: colorItem,
     }),
     {
-      boxShadow: `0 6px ${vars.colors[darkColor]}`,
+      boxShadow: `0 6px ${vars.colors[darkColor as keyof typeof vars.colors]}`,
       ':active': {
         transform: 'translateY(4px)',
-        boxShadow: `0 2px ${vars.colors[darkColor]}`,
+        boxShadow: `0 2px ${vars.colors[darkColor as keyof typeof vars.colors]}`,
       },
     },
   ]);
@@ -37,8 +37,8 @@ const sizeVariants = {
       marginX: '2x',
     }),
     {
-      width: '96px',
-      height: '36px',
+      width: constants.COLOREDBUTTON_SIZE_PIXEL.small.width,
+      height: constants.COLOREDBUTTON_SIZE_PIXEL.small.height,
     },
   ]),
   medium: style([
@@ -47,8 +47,8 @@ const sizeVariants = {
       padding: '4x',
     }),
     {
-      width: '130px',
-      height: '44px',
+      width: constants.COLOREDBUTTON_SIZE_PIXEL.medium.width,
+      height: constants.COLOREDBUTTON_SIZE_PIXEL.medium.height,
     },
   ]),
   large: style([
@@ -57,8 +57,8 @@ const sizeVariants = {
       padding: '4x',
     }),
     {
-      width: '192px',
-      height: '64px',
+      width: constants.COLOREDBUTTON_SIZE_PIXEL.large.width,
+      height: constants.COLOREDBUTTON_SIZE_PIXEL.large.height,
     },
   ]),
 };
@@ -94,7 +94,7 @@ export const textStyle = style([
   }),
   sprinkles({ position: 'relative' }),
   {
-    height: '100%',
-    width: '100%',
+    height: 'full',
+    width: 'full',
   },
 ]);
