f3f6a2ac71b640779457cff4fce5d027e4143630
minjeong <minj_1203@naver.com>
2024-03-15 16:24:34 +0900

    refactor: 중복되는 코드를 함수로 만들어 적용함

--- frontend/src/pages/loading/index.tsx
+++ frontend/src/pages/loading/index.tsx
@@ -4,26 +4,23 @@ import * as styles from './index.css.ts';
 
 import BasicContentFrame from '@/components/BasicContentFrame/index.tsx';
 
-import {
-  LOADING_IMAGE,
-  LOADING_MESSAGE,
-  LOADING_TOP,
-} from '@/pages/loading/constants.ts';
+import * as constants from '@/pages/loading/constants.ts';
 
 const Loading = () => {
   const [messageId, setMessageId] = useState<string>('');
   const [message, setMessage] = useState<string>('');
 
-  useEffect(() => {
-    let random = Math.floor(Math.random() * LOADING_MESSAGE.length);
-    setMessageId(LOADING_MESSAGE[random].messageId);
-    setMessage(LOADING_MESSAGE[random].message);
+  const randomMessage = () => {
+    const random = Math.floor(Math.random() * constants.LOADING_MESSAGE.length);
+    setMessageId(constants.LOADING_MESSAGE[random].messageId);
+    setMessage(constants.LOADING_MESSAGE[random].message);
+  };
 
+  useEffect(() => {
+    randomMessage();
     const interval = setInterval(() => {
-      random = Math.floor(Math.random() * LOADING_MESSAGE.length);
-      setMessageId(LOADING_MESSAGE[random].messageId);
-      setMessage(LOADING_MESSAGE[random].message);
-    }, 3000);
+      randomMessage();
+    }, constants.LOADING_MESSAGE_DELAY);
 
     return () => clearInterval(interval);
   }, []);
@@ -31,8 +28,8 @@ const Loading = () => {
   return (
     <BasicContentFrame>
       <div className={styles.loadingWrapper}>
-        <div className={styles.loadingNumFont}>{LOADING_TOP}</div>
-        <img src={LOADING_IMAGE} alt='loading' />
+        <div className={styles.loadingNumFont}>{constants.LOADING_TOP}</div>
+        <img src={constants.LOADING_IMAGE} alt='loading' />
         <div className={styles.loadingMessageWrapper}>
           <div className={styles.loadingNumFont}># {messageId}</div>
           <div className={styles.loadingMessage}>{message}</div>
