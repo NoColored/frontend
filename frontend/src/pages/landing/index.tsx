353e511df69e554b8defd118bc935194df33eb18
minjeong <minj_1203@naver.com>
2024-03-21 12:15:55 +0900

    feat: 게스트 로그인 기능 추가

--- frontend/src/pages/landing/index.tsx
+++ frontend/src/pages/landing/index.tsx
@@ -1,11 +1,28 @@
 import { useNavigate } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
 
 import * as styles from './index.css';
 
 import ColoredButton from '@/components/button/ColoredButton/index';
 
+import { getGuestLogin } from '@/services/auth';
+
+import { tokenState, nickNameState, userCodeState } from '@/states/auth';
+
 const Landing = () => {
   const navigate = useNavigate();
+  const [token, setToken] = useRecoilState(tokenState);
+  const [nickName, setNickName] = useRecoilState(nickNameState);
+  const [userCode, setUserCode] = useRecoilState(userCodeState);
+
+  const clickGuestLogin = async () => {
+    const guest = await getGuestLogin();
+    if (guest) {
+      setToken(guest.data.token);
+      setNickName(guest.data.nickName);
+      setUserCode(guest.data.userCode);
+    }
+  };
 
   const clickLogIn = () => {
     navigate('/login');
@@ -24,7 +41,7 @@ const Landing = () => {
           text='GUEST'
           color='gray300'
           size='large'
-          onClick={() => {}}
+          onClick={clickGuestLogin}
         />
         <ColoredButton
           text='LOG IN'
--- frontend/src/pages/landing/logIn/index.tsx
+++ frontend/src/pages/landing/logIn/index.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 import ColoredButton from '@/components/button/ColoredButton/index';
 import InputTextBox from '@/components/textbox/InputTextBox/index';
@@ -8,41 +7,16 @@ import useModal from '@/hooks/useModal';
 
 import * as styles from '@/pages/landing/index.css';
 import LogInFail from '@/pages/landing/logIn/LogInFail';
-import { requestLogIn } from '@/pages/landing/logIn/requestLogIn';
 import SignUp from '@/pages/landing/logIn/SignUp';
 
 const LogIn = () => {
-  const [userId, setUserId] = useState<string>('');
-  const [userPassword, setUserPassword] = useState<string>('');
   const [isClicked, setIsClicked] = useState(false);
 
-  const navigate = useNavigate();
-
   const { Modal, openModal, closeModal } = useModal();
-
-  const handleIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserId(e.target.value);
-  };
-
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserPassword(e.target.value);
-  };
 
   const handleSignUpClick = () => {
     openModal();
     setIsClicked(true);
-  };
-
-  const handleLogInClick = () => {
-    const check = requestLogIn(userId, userPassword);
-
-    // userId 와 userPassword null 여부는 백에서도 체크
-    if (!userId || !userPassword || !check) {
-      openModal();
-      setIsClicked(false);
-    } else {
-      navigate('/');
-    }
   };
 
   return (
@@ -52,15 +26,13 @@ const LogIn = () => {
         type='text'
         placeholder='아이디'
         size='large'
-        value={userId}
-        onChange={handleIdChange}
+        onChange={() => {}}
       />
       <InputTextBox
         type='password'
         placeholder='비밀번호'
         size='large'
-        value={userPassword}
-        onChange={handlePasswordChange}
+        onChange={() => {}}
       />
       <div className={styles.buttonWrapper}>
         <ColoredButton
@@ -73,7 +45,7 @@ const LogIn = () => {
           text='LOG IN'
           color='pink'
           size='large'
-          onClick={handleLogInClick}
+          onClick={() => {}}
         />
       </div>
       <Modal>
