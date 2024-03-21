0e143aa7829e982b4846155e89fc661289473dec
김세진 <sejin1398@gmail.com>
2024-03-21 15:44:27 +0900

    feat: lobby 설정 정보 추가
    - 설정 변경 버튼
    - 대기실 코드
    - 비밀 번호
    
    related to : #S10P22A608-147

--- frontend/src/pages/play/lobby/index.tsx
+++ frontend/src/pages/play/lobby/index.tsx
@@ -1,16 +1,24 @@
 import * as styles from './index.css';
 
-import BasicContentFrame from '@/components/BasicContentFrame/WithButtons/index';
+import BasicContentFrame from '@/components/BasicContentFrame/WithButtons';
+import ColoredTextBox from '@/components/textbox/ColoredTextBox';
 
 import Players from '@/pages/play/lobby/Players';
+import SettingButton from '@/pages/play/lobby/SettingButton';
 
 const Lobby = () => {
+  const code = '1234';
+  const password = '1234';
+
   return (
-    <BasicContentFrame>
+    <BasicContentFrame backButtonLabel='나가기'>
       <div className={styles.lobby}>
         <div className={styles.settings}>
-          <div>코드번호</div>
-          <div>비밀번호</div>
+          <SettingButton />
+          <div className={styles.code}>{code}</div>
+          <ColoredTextBox size='small' color='red' text='코드번호' />
+          <div className={styles.code}>{password}</div>
+          <ColoredTextBox size='small' color='red' text='비밀번호' />
         </div>
         <div className={styles.lobbyInfo}>
           <div className={styles.lobbyTitle}>방제목최대9글자</div>
