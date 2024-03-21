6a02d640be739184b51a64f3070246f8d0470184
minjeong <minj_1203@naver.com>
2024-03-21 14:59:02 +0900

    fix: token 이름 수정

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -5,7 +5,7 @@ import { api } from '@/services/index';
 export const getGuestLogin = async () => {
   try {
     const response = await api.get<User>(false, '/user/guest');
-    localStorage.setItem('accessToken', response.data.token);
+    localStorage.setItem('token', response.data.token);
     return response.data;
   } catch (e) {
     console.log(e);
