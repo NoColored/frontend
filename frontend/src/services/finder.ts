8d59e449e20d109cfed235ad1aca2f914b99bee7
minjeong <minj_1203@naver.com>
2024-03-25 09:55:04 +0900

    feat: 대기실 리스트 조회 api 구현 중 필요한 로직만 남겨놓음

--- frontend/src/services/finder.ts
+++ frontend/src/services/finder.ts
@@ -1,24 +1,10 @@
-import type { RoomInfo, RoomListInfo, RoomRequest } from '@/types/play';
+import type { RoomListInfo } from '@/types/play';
 
 import { api } from '@/services/index';
 
 export const getRoomList = async () => {
   try {
-    const response = await api.get<RoomListInfo>(true, 'play/friendly/{}');
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    return null;
-  }
-};
-
-export const postCreateRoom = async (roomRequest: RoomRequest) => {
-  try {
-    const response = await api.post<RoomInfo, RoomRequest>(
-      true,
-      'play/friendly',
-      roomRequest,
-    );
+    const response = await api.get<RoomListInfo>(true, 'play/friendly/`{1}`');
     return response.data;
   } catch (error) {
     console.log(error);
