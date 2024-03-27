c4cd24526008e3e9e66653a88efff874b1ee8c46
minjeong <minj_1203@naver.com>
2024-03-27 10:47:23 +0900

    feat: 비밀번호 글자 제한 기능 추가

--- frontend/src/pages/play/finder/Modal/PasswordModal.tsx
+++ frontend/src/pages/play/finder/Modal/PasswordModal.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import ColoredButton from '@/components/button/ColoredButton';
 import InputTextBox from '@/components/textbox/InputTextBox';
 
@@ -9,16 +11,26 @@ interface Props {
 }
 
 const PasswordModal = ({ buttonLeft, buttonRight }: Props) => {
+  const [roomPassword, setRoomPassword] = useState<string>('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    if (name === 'roomPassword' && value.length > 4) return;
+    setRoomPassword(value);
+  };
+
   return (
     <div className={styles.modalWrapper}>
       <div className={styles.contentBox}>
         <span>이 방의 Colored들은 </span>
         <span>아무하고나 쉽게 싸우지 않습니다 </span>
         <InputTextBox
+          name='roomPassword'
           placeholder='비밀번호 4자리를 입력하세요'
           size='small'
           type='text'
-          onChange={() => {}}
+          value={roomPassword}
+          onChange={handleChange}
         />
         <div className={styles.modalTwoButtonWrapper}>
           <ColoredButton
