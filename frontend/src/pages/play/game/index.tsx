72b0ffdaa824c7fef71ed380dd8c8bb6de425136
damdam6 <dambi626626@gmail.com>
2024-03-28 16:53:53 +0900

    fix: 임시 소켓 오픈을 위한 체크 삭제
    related to: #S10P22A608-233

--- frontend/src/pages/play/game/index.tsx
+++ frontend/src/pages/play/game/index.tsx
@@ -2,13 +2,9 @@ import { useEffect } from 'react';
 
 import BasicContentFrame from '@/components/BasicContentFrame';
 
-import { useWebSocket } from '@/hooks/useWebSocket';
-
 import { config } from '@/game/scene/config';
 
 const Game = () => {
-  useWebSocket();
-
   useEffect(() => {
     const game = new Phaser.Game(config);
 
