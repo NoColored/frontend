c3fb744d82aed78c2ad4b85286d313e635339cc2
minjeong <minj_1203@naver.com>
2024-03-28 10:22:08 +0900

    feat: RoundCornerImageBox 스킨 이미지 추가

--- frontend/src/pages/play/mode/MatchingButton.tsx
+++ frontend/src/pages/play/mode/MatchingButton.tsx
@@ -1,19 +1,25 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+import * as styles from './index.css';
 
 import ColoredButton from '@/components/button/ColoredButton';
 import ColoredIconButton from '@/components/button/ColoredIconButton';
+import RoundCornerImageBox from '@/components/imagebox/RoundCornerImageBox';
 
 import useModal from '@/hooks/useModal';
 import { useWebSocket } from '@/hooks/useWebSocket';
 
 import * as constants from '@/pages/play/mode/constants';
 
+import { getUser } from '@/services/auth';
 import { deleteMatching, getMatching } from '@/services/matching';
 
 import { ROUTE } from '@/router/constants';
 
 const MatchingButton = () => {
   const navigate = useNavigate();
+  const [skin, setSkin] = useState<string>();
   const { Modal, openModal, closeModal } = useModal();
 
   const handleWebSocketMessage = (message: WebSocketMessage<actionType>) => {
@@ -28,6 +34,13 @@ const MatchingButton = () => {
   useWebSocket(handleWebSocketMessage);
 
   const startMatching = async () => {
+    const userInfo = await getUser();
+    if (!userInfo) {
+      setSkin(undefined);
+    } else {
+      setSkin(userInfo.skin);
+    }
+
     const matchingSuccess = await getMatching();
     if (matchingSuccess) {
       openModal();
@@ -47,13 +60,25 @@ const MatchingButton = () => {
       />
 
       <Modal>
-        <div>매칭중</div>
-        <ColoredButton
-          size='small'
-          text='취소'
-          color='navy'
-          onClick={deleteMatching}
-        />
+        <div className={styles.matchingModalWrapper}>
+          <div className={styles.matchingMessage}>
+            {constants.matchingMessage}
+          </div>
+          <div className={styles.matchingImageWrapper}>
+            <RoundCornerImageBox
+              size='large'
+              imgSrc={skin}
+              borderColor='black'
+              borderSize='5x'
+            />
+          </div>
+          <ColoredButton
+            size='small'
+            text='취소'
+            color='navy'
+            onClick={deleteMatching}
+          />
+        </div>
       </Modal>
     </>
   );
