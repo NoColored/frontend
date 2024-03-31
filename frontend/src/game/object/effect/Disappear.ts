83b828310ebd8fc2d1f9f35b6f3ae991e1316899
damdam6 <dambi626626@gmail.com>
2024-04-01 00:27:38 +0900

    feat: Disappear setDepth수정

--- frontend/src/game/object/effect/Disappear.ts
+++ frontend/src/game/object/effect/Disappear.ts
@@ -1,8 +1,10 @@
+import * as constants from '@/game/constants';
+
 export class Disappear extends Phaser.GameObjects.Sprite {
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y, 'disappear');
     scene.add.existing(this);
-    this.play('disappear');
+    this.play('disappear').setDepth(constants.INGAME_DEPTH.EFFECT);
     this.once('animationcomplete', () => this.destroy());
   }
 }
