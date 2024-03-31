50c623ed356c79ab2d67a26e1c22abc1ff7ee6c0
damdam6 <dambi626626@gmail.com>
2024-03-31 23:29:12 +0900

    feat: blowup Effect 구현

--- frontend/src/game/object/effect/Blowup.ts
+++ frontend/src/game/object/effect/Blowup.ts
@@ -1,8 +1,12 @@
+import * as constants from '@/game/constants';
+
 export class Blowup extends Phaser.GameObjects.Sprite {
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y, 'blowup');
     scene.add.existing(this);
     this.play('blowupAnim');
     this.once('animationcomplete', () => this.destroy());
+    this.setDepth(constants.INGAME_DEPTH.EFFECT);
+    this.scene.sound.add('blowupSound', { volume: 1 }).play();
   }
 }
