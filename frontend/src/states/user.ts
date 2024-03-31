10b0a2c99b82636fb507576134f92ca2a5bdeb9e
김세진 <sejin1398@gmail.com>
2024-04-01 00:42:23 +0900

    fix: zustand 수정
    - 회원 정보 로드 시 값을 저장하도록 수정
    - 새로고침 시 값이 남아있도록 수정

--- frontend/src/states/user.ts
+++ frontend/src/states/user.ts
@@ -1,17 +1,23 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface UserState {
   isGuest: boolean;
-  setGuest: () => void;
-  setMember: () => void;
+  setIsGuest: (isGuest: boolean) => void;
+  userCode: string;
+  setUserCode: (userCode: string) => void;
 }
 
-export const useUserStateStore = create<UserState>((set) => ({
-  isGuest: false,
-  setGuest: () => {
-    set({ isGuest: true });
-  },
-  setMember: () => {
-    set({ isGuest: false });
-  },
-}));
+export const useUserStateStore = create<UserState>()(
+  persist(
+    (set) => ({
+      isGuest: false,
+      setIsGuest: (isGuest) => set({ isGuest }),
+      userCode: '',
+      setUserCode: (userCode) => set({ userCode }),
+    }),
+    {
+      name: 'user-state',
+    },
+  ),
+);
