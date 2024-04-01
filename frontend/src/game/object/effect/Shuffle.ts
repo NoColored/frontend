465f32ff29c1e262b881b4e3a8774e1c91d145bc
damdam6 <dambi626626@gmail.com>
2024-04-01 16:01:49 +0900

    feat: gradient 이미지 - 화살표 배경으로 세팅

--- frontend/src/game/object/effect/Shuffle.ts
+++ frontend/src/game/object/effect/Shuffle.ts
@@ -7,10 +7,20 @@ export class Shuffle extends Phaser.GameObjects.Sprite {
     super(scene, width / 2, height / 2, 'shuffle');
     scene.add.existing(this);
     this.play('shuffleAnim').setSize(350, 350);
+    const back = this.scene.add
+      .image(width / 2, height / 2, 'gradient')
+      .setDepth(constants.INGAME_DEPTH.EFFECT - 1);
+
+    const maskGraphics = this.scene.make.graphics({ x: 0, y: 0 });
+    maskGraphics.fillRoundedRect(width / 2, height / 2, 350, 350, 50);
+    maskGraphics.closePath();
+    const mask = maskGraphics.createGeometryMask();
+    back.setMask(mask);
+
     this.setDepth(constants.INGAME_DEPTH.EFFECT);
 
     this.scene.time.addEvent({
-      delay: 3000,
+      delay: 1500,
       callback: () => this.destroy(),
       loop: false,
     });
