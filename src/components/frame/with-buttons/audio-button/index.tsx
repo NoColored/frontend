abadfe2d530e18d46525f06ffeadf665edb2cc11
전민정 <minj_1203@naver.com>
2025-06-24 15:24:09 +0900

    :recycle: mute state 삭제 #4

--- src/components/frame/with-buttons/audio-button/index.tsx
+++ src/components/frame/with-buttons/audio-button/index.tsx
@@ -1,21 +1,23 @@
 import { Volume2, VolumeX } from 'lucide-react';
+
 import * as styles from './index.css';
-import { useState } from 'react';
+
 import { useAudioSetting } from '@/hooks/useAudioSetting';
 
 const AudioButton = () => {
-  const [isMute, setIsMute] = useState(false);
-  const { backgroundSound, effectSound, saveSettings } = useAudioSetting();
+  const { backgroundSound, saveSettings } = useAudioSetting();
 
   const handleClick = () => {
-    setIsMute(!isMute);
-    saveSettings(!backgroundSound, !effectSound);
+    saveSettings(!backgroundSound, !backgroundSound);
   };
 
   return (
     <button type='button' className={styles.button} onClick={handleClick}>
-      {!isMute && <Volume2 className={styles.svg} />}
-      {isMute && <VolumeX className={styles.svg} />}
+      {backgroundSound ? (
+        <Volume2 className={styles.svg} />
+      ) : (
+        <VolumeX className={styles.svg} />
+      )}
     </button>
   );
 };
