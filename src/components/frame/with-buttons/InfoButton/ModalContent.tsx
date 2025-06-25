e1920f2818a339cf799b2521db91ad8b4b0806db
nijesmik <bkybb@duck.com>
2025-06-25 18:45:25 +0900

    :bug: props 타입 수정

--- src/components/frame/with-buttons/InfoButton/ModalContent.tsx
+++ src/components/frame/with-buttons/InfoButton/ModalContent.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 
-import { indexProps } from './types';
-
 import GameInfo from '@/components/frame/with-buttons/InfoButton/GameInfo';
 import ItemInfo from '@/components/frame/with-buttons/InfoButton/ItemInfo';
 import MainInfo from '@/components/frame/with-buttons/InfoButton/MainInfo';
 import TierInfo from '@/components/frame/with-buttons/InfoButton/TierInfo';
 import type { infoType } from '@/components/frame/with-buttons/InfoButton/types';
 
-const ModalContent = ({ onClose }: indexProps) => {
+interface Props {
+  onClose: () => void;
+}
+
+const ModalContent = ({ onClose }: Props) => {
   const [currentView, setCurrentView] = useState<infoType>('main');
 
   const handleBack = () => {
