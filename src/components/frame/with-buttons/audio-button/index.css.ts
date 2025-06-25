e04e0fbe60939d515e670cfa8c9be353b397c79c
nijesmik <bkybb@duck.com>
2025-06-25 20:02:19 +0900

    :lipstick: 공통 버튼 스타일 사용

--- src/components/frame/with-buttons/audio-button/index.css.ts
+++ src/components/frame/with-buttons/audio-button/index.css.ts
@@ -1,33 +1,6 @@
 import { style } from '@vanilla-extract/css';
 
-import { borderDarkOptions, flexOptions } from '@/styles/common.css';
-import { sprinkles } from '@/styles/sprinkles.css';
-import { vars } from '@/styles/vars.css';
-
-const BUTTON_SIZE = '24px';
 const ICON_SIZE = '16px';
-
-export const button = style([
-  flexOptions({ option: 'center' }),
-  borderDarkOptions({ color: 'black', width: '1x' }),
-  sprinkles({
-    borderRadius: 'full',
-    backgroundColor: 'white',
-    marginX: '0.5x',
-  }),
-  {
-    width: `${BUTTON_SIZE}`,
-    height: `${BUTTON_SIZE}`,
-
-    ':hover': {
-      backgroundColor: vars.colors.gray100,
-    },
-
-    ':active': {
-      backgroundColor: vars.colors.gray200,
-    },
-  },
-]);
 
 export const svg = style({
   objectFit: 'contain',
