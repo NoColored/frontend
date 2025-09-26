6c0ee2b163dd7e0675760097b70cfde0563f06f3
nijesmik <bkybb@duck.com>
2025-09-26 20:42:51 +0900

    :recycle: replace `states/user` with `features/user` and update references

--- src/features/sign-up/ui.tsx
+++ src/features/sign-up/ui.tsx
@@ -12,10 +12,9 @@ import * as constants from '@/pages/landing/logIn/constants';
 import { getIdCheck, postGuestSignUp, postSignUp } from '@/services/auth';
 import { setFullScreen } from '@/services/landing';
 
-import { GUEST, NOT_LOGIN, useUserStateStore } from '@/states/user';
-
 import { checkSignUpInfo } from '@/utils/useSignUp';
 
+import { USER_STATUS, useUserStore } from '@/features/user';
 import { ROUTE } from '@/router/constants';
 
 interface Props {
@@ -23,7 +22,7 @@ interface Props {
 }
 
 const SignUp = ({ closeModal }: Props) => {
-  const { loginStatus } = useUserStateStore.getState();
+  const { loginStatus } = useUserStore.getState();
   const navigate = useNavigate();
   const [errorMessage, setErrorMessage] = useState(
     constants.ERROR_MESSAGE.welcome,
@@ -56,7 +55,7 @@ const SignUp = ({ closeModal }: Props) => {
       return;
     }
 
-    if (loginStatus === GUEST) {
+    if (loginStatus === USER_STATUS.guest) {
       await postGuestSignUp(signUpInfo).then((isSuccess) => {
         if (isSuccess) {
           closeModal();
@@ -66,7 +65,7 @@ const SignUp = ({ closeModal }: Props) => {
       return;
     }
 
-    if (loginStatus === NOT_LOGIN) {
+    if (loginStatus === USER_STATUS.notLoggedIn) {
       await postSignUp(signUpInfo).then((isSuccess) => {
         if (isSuccess) {
           closeModal();
--- src/states/user.ts
+++ src/states/user.ts
@@ -1,5 +1,1 @@
-export const NOT_LOGIN = 0;
-export const GUEST = 1;
-export const USER = 2;
-
 export { useUserStore as useUserStateStore } from '@/features/user';
