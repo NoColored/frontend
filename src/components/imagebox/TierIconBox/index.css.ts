98e6a750e1a9dc2160ee8cf374242ebbd53e020b
nijesmik <bkybb@duck.com>
2025-10-05 02:26:50 +0900

    :recycle: refactor `TierIconBox` to use `variant` function and simplify style definitions #32

--- src/components/imagebox/TierIconBox/index.css.ts
+++ src/components/imagebox/TierIconBox/index.css.ts
@@ -1,10 +1,11 @@
 import { style } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
 
-import * as constants from '@/components/imagebox/constants';
-import type { tierSizeType } from '@/components/imagebox/types';
+import { variant } from '@/styles/utils';
 
-const tierBoxBase = style([
+import { TIER } from '@/constants/tier';
+
+const base = style([
   {
     overflow: 'hidden',
     aspectRatio: '1 / 1',
@@ -16,33 +17,29 @@ const tierBoxBase = style([
   },
 ]);
 
-const tierVariants: {
-  [tier in (typeof constants.TIER_RANGE)[number]]?: ReturnType<typeof style>;
-} = {};
+const tier = variant([...TIER], (name) =>
+  style({
+    backgroundImage: `url('/images/ui/icon/tier/icon-tier-${name}.svg')`,
+  }),
+);
 
-constants.TIER_RANGE.forEach((tier) => {
-  tierVariants[tier] = style({
-    backgroundImage: `url('/images/ui/icon/tier/icon-tier-${tier}.svg')`,
-  });
-});
-
-const sizeVariants = Object.keys(constants.IMAGEBOX_SIZE).reduce(
-  (variants, sizeKey) => {
-    const size = sizeKey as tierSizeType;
-    variants[size] = style([
-      {
-        height: constants.IMAGEBOX_SIZE[size],
-      },
-    ]);
-    return variants;
-  },
-  {} as Record<tierSizeType, ReturnType<typeof style>>,
+const size = variant(
+  {
+    xsmall: '32px',
+    small: '48px',
+    medium: '96px',
+    full: '100%',
+  } as const,
+  ([_, value]) =>
+    style({
+      height: value,
+    }),
 );
 
 export const tierBox = recipe({
-  base: tierBoxBase,
+  base,
   variants: {
-    tier: tierVariants,
-    size: sizeVariants,
+    tier,
+    size,
   },
 });
