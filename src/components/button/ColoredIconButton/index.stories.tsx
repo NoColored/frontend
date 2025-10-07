5182ab4172e5827266dabd179a0b98b93cf7b01e
nijesmik <bkybb@duck.com>
2025-10-07 19:16:30 +0900

    :sparkles: `ColoredIconButton` 스토리 추가 #35

--- src/components/button/ColoredIconButton/index.stories.tsx
+++ src/components/button/ColoredIconButton/index.stories.tsx
@@ -35,6 +35,12 @@ const meta = {
       control: 'radio',
       options: Object.keys(sizeVariants),
     },
+    icon: {
+      control: false,
+    },
+    text: {
+      control: 'text',
+    },
   },
   args: { onClick: fn() },
 } satisfies Meta<typeof ColoredIconButton>;
@@ -43,12 +49,12 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Color: Story = {
+export const Play: Story = {
   args: {
     size: 'medium',
     color: 'red',
     icon: '/images/ui/icon/button/icon-button-play-h50w50.png',
-    text: 'play',
+    text: 'Play',
   },
 };
 
@@ -61,20 +67,63 @@ export const Collection: Story = {
   },
 };
 
-export const Play: Story = {
+export const Ranking: Story = {
   args: {
-    size: 'large',
-    color: 'pink',
-    icon: '/images/ui/icon/shape/icon-shape-white-big-player0-h48w48.png',
-    text: '게임시작',
+    size: 'medium',
+    color: 'green',
+    icon: '/images/ui/icon/button/icon-button-ranking-h50w50.png',
+    text: 'Ranking',
   },
 };
 
-export const Ranking: Story = {
+export const FriendlyMatch: Story = {
+  args: {
+    size: 'xlarge',
+    color: 'pink',
+    icon: '/images/ui/icon/button/icon-button-modemulti-h50w50.png',
+    text: '친선전',
+  },
+};
+
+export const RankedMatch: Story = {
   args: {
     size: 'xlarge',
     color: 'yellow',
     icon: '/images/ui/icon/button/icon-button-moderank-h50w50.png',
     text: '랭킹전',
+  },
+};
+
+const width = '200px';
+
+export const GameReady: Story = {
+  args: {
+    size: 'large',
+    color: 'gray300',
+    icon: '/images/ui/icon/shape/icon-shape-white-big-player1-h48w48.png',
+    text: '게임준비',
+  },
+  render: (args: Required<Story['args']>) => {
+    return (
+      <div style={{ width }}>
+        <ColoredIconButton {...args} />
+      </div>
+    );
+  },
+};
+
+export const GameStart: Story = {
+  args: {
+    size: 'large',
+    color: 'blue',
+    icon: '/images/ui/icon/shape/icon-shape-white-big-player3-h48w48.png',
+    text: '게임시작',
+  },
+  render: (args: Required<Story['args']>) => {
+    return (
+      <div style={{ width }}>
+        <ColoredIconButton {...args} />
+      </div>
+    );
   },
 };
