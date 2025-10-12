cde2aa555947e9d242d658138ef0e771a378aacf
nijesmik <bkybb@duck.com>
2025-10-12 21:03:52 +0900

    :recycle: input 컴포넌트 스타일 리팩토링 #42

--- src/components/input/index.css.ts
+++ src/components/input/index.css.ts
@@ -1,9 +1,8 @@
 import { style } from '@vanilla-extract/css';
 import { recipe } from '@vanilla-extract/recipes';
 
-import { INPUT_STYLE } from './constants';
+import { size } from './variants.css';
 
-import { borderDarkOptions } from '@/styles/common.css';
 import { sprinkles } from '@/styles/sprinkles.css';
 import { vars } from '@/styles/vars.css';
 
@@ -14,80 +13,15 @@ const base = style([
     color: 'white',
   }),
   {
-    textAlign: 'left',
     '::placeholder': {
       color: vars.colors.white,
     },
   },
 ]);
 
-export const size = {
-  widthFull: style([
-    borderDarkOptions({ color: 'gray500', width: '3x' }),
-    sprinkles({
-      fontSize: '1x',
-      backgroundColor: 'gray500',
-    }),
-    {
-      textAlign: 'center',
-      width: `100%`,
-      height: INPUT_STYLE.small.height,
-    },
-  ]),
-  small: style([
-    borderDarkOptions({ color: 'gray500', width: '3x' }),
-    sprinkles({
-      fontSize: '1x',
-      backgroundColor: 'gray500',
-      marginY: '2x',
-    }),
-    {
-      textAlign: 'center',
-      width: INPUT_STYLE.small.width,
-      height: INPUT_STYLE.small.height,
-    },
-  ]),
-  medium: style([
-    borderDarkOptions({ color: 'gray300', width: '3x' }),
-    sprinkles({
-      fontSize: '1.5x',
-      paddingX: '4x',
-      marginY: '1x',
-      backgroundColor: 'gray300',
-    }),
-    {
-      '@media': {
-        'screen and (max-height: 365px)': {
-          width: INPUT_STYLE.small.width,
-          height: INPUT_STYLE.small.height,
-        },
-      },
-      width: INPUT_STYLE.medium.width,
-      height: INPUT_STYLE.medium.height,
-    },
-  ]),
-  large: style([
-    borderDarkOptions({ color: 'navy', width: '5x' }),
-    sprinkles({
-      fontSize: '2x',
-      paddingX: '4x',
-      marginY: INPUT_STYLE.large.marginY,
-      backgroundColor: 'navy',
-    }),
-    {
-      width: INPUT_STYLE.large.width,
-      height: INPUT_STYLE.large.height,
-    },
-  ]),
-};
-
-const input = recipe({
+export const input = recipe({
   base,
   variants: {
     size,
   },
 });
-
-export const styles = {
-  input,
-};
