f61b5752d182471d2b1bac163ff61a9bc686ea18
전민정 <minj_1203@naver.com>
2025-10-19 20:03:56 +0900

    :sparkles: add notice site url (#55)

--- src/components/frame/with-buttons/menu-button/constants.ts
+++ src/components/frame/with-buttons/menu-button/constants.ts
@@ -3,3 +3,6 @@ export const MENU_ID = {
   gameInfo: 'game-info',
   guest: 'become-member',
 };
+
+export const NOTICE_URL =
+  'https://www.notion.so/nijesmik/NoColored-28c2fe008f9880dfa220c1a5fbbd4111?source=copy_link';
