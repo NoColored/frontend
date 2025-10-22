f3351e2bab0f799d84edd2079af293b811f515f8
nijesmik <bkybb@duck.com>
2025-10-23 04:52:29 +0900

    :fire: `MainInfo` 컴포넌트 삭제

--- src/components/frame/with-buttons/menu-button/game-info/index.tsx
+++ src/components/frame/with-buttons/menu-button/game-info/index.tsx
@@ -1,22 +1,29 @@
 import { useState } from 'react';
 
 import GameRules from './GameRules';
+import * as styles from './index.css';
 import ItemInfo from './ItemInfo';
-import MainInfo from './MainInfo';
 import TierInfo from './TierInfo';
 
-import type { infoType } from './types';
+import ColoredButton from '@/components/button/ColoredButton';
+import SettingTextButton from '@/components/button/SettingTextButton';
+
+const menu = {
+  game: '게임 방식',
+  item: '아이템',
+  tier: '티어',
+};
+
+type infoType = keyof typeof menu;
 
 interface Props {
   onClose: () => void;
 }
 
 const GameInfo = ({ onClose }: Props) => {
-  const [currentView, setCurrentView] = useState<infoType>('main');
+  const [currentView, setCurrentView] = useState<infoType | 'main'>('main');
 
-  const handleBack = () => {
-    setCurrentView('main');
-  };
+  const handleBack = () => setCurrentView('main');
 
   if (currentView === 'game') {
     return <GameRules onBack={handleBack} onClose={onClose} />;
@@ -27,7 +34,33 @@ const GameInfo = ({ onClose }: Props) => {
   if (currentView === 'tier') {
     return <TierInfo onBack={handleBack} onClose={onClose} />;
   }
-  return <MainInfo onClose={onClose} onNavigate={setCurrentView} />;
+  return (
+    <>
+      <div className={styles.text}>
+        <div> 게임 정보</div>
+      </div>
+      <div className={styles.boxWrapper}>
+        {Object.entries(menu).map(([key, title]) => (
+          <SettingTextButton
+            key={key}
+            onClick={() => setCurrentView(key as infoType)}
+            size='medium'
+            colorStyle='black'
+          >
+            {title}
+          </SettingTextButton>
+        ))}
+      </div>
+      <div className={styles.buttonWrapper}>
+        <ColoredButton
+          text='닫기'
+          color='green'
+          size='small'
+          onClick={onClose}
+        />
+      </div>
+    </>
+  );
 };
 
 export default GameInfo;
--- src/components/frame/with-buttons/menu-button/game-info/types.d.ts
+++ src/components/frame/with-buttons/menu-button/game-info/types.d.ts
@@ -1,8 +1,4 @@
-import { INFO_TYPE } from './constants';
-
 export interface indexProps {
   onClose: () => void;
   onBack: () => void;
 }
-
-type infoType = (typeof INFO_TYPE)[number];
