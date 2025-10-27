3577a8db8d3c3db4d43c25b80ae5cae6cd3dc8d7
전민정 <minj_1203@naver.com>
2025-10-27 19:21:37 +0900

    :bulb: remove comments (#72)

--- src/pages/play/mode/api.ts
+++ src/pages/play/mode/api.ts
@@ -1,33 +1,25 @@
 import { client } from '@/shared/api';
 
 export const getMatching = async () => {
-  return (
-    client
-      .get<string>('/play/ranking')
-      .then((response) => {
-        // console.log('매칭 요청 성공');
-        return response.status === 200;
-      })
-      // eslint-disable-next-line
-      .catch((error) => {
-        console.error(error);
-        return false;
-      })
-  );
+  return client
+    .get<string>('/play/ranking')
+    .then((response) => {
+      return response.status === 200;
+    })
+    .catch((error) => {
+      console.error(error);
+      return false;
+    });
 };
 
 export const deleteMatching = async () => {
-  return (
-    client
-      .delete<string>('/play/ranking')
-      .then((response) => {
-        // console.log('매칭 취소 요청 성공');
-        return response.status === 200;
-      })
-      // eslint-disable-next-line
-      .catch((error) => {
-        console.error(error);
-        return false;
-      })
-  );
+  return client
+    .delete<string>('/play/ranking')
+    .then((response) => {
+      return response.status === 200;
+    })
+    .catch((error) => {
+      console.error(error);
+      return false;
+    });
 };
